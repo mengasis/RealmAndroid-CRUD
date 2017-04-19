dbff3e835a684aae7f2557811f4dbd58bb9a8ba9
Manuel Venegas <mengasis@outlook.com>
2017-04-19 20:37:27 -0300

    Configuracion de la DB Realm

--- app/src/main/java/com/mengasis/manuel/exampleandroidrealm/MainActivity.java
+++ app/src/main/java/com/mengasis/manuel/exampleandroidrealm/MainActivity.java
@@ -3,11 +3,22 @@ package com.mengasis.manuel.exampleandroidrealm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name("Hero_Database")
+                .schemaVersion(1)
+                .build();
+
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 }
