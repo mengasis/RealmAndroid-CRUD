7364e07575102f6cdabb09e403b482dfaac55d97
Manuel Venegas <mengasis@outlook.com>
2017-04-19 20:34:41 -0300

    Preparados los archivos

---
+++ app/src/main/java/com/mengasis/manuel/exampleandroidrealm/Models/Hero.java
@@ -0,0 +1,8 @@
+package com.mengasis.manuel.exampleandroidrealm.Models;
+
+/**
+ * Created by manuel on 19-04-17.
+ */
+
+public class Hero {
+}
---
+++ app/src/main/java/com/mengasis/manuel/exampleandroidrealm/Services/HeroService.java
@@ -0,0 +1,8 @@
+package com.mengasis.manuel.exampleandroidrealm.Services;
+
+/**
+ * Created by manuel on 19-04-17.
+ */
+
+public class HeroService {
+}
