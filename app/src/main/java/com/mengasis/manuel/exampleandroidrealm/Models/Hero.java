2e72e8fe281e5e318ef8ead1e64bd1a4a328ed14
Manuel Venegas <mengasis@outlook.com>
2017-04-19 21:40:15 -0300

    Modelo Hero Realm y Servicio con Create y Read

--- app/src/main/java/com/mengasis/manuel/exampleandroidrealm/Models/Hero.java
+++ app/src/main/java/com/mengasis/manuel/exampleandroidrealm/Models/Hero.java
@@ -1,8 +1,33 @@
 package com.mengasis.manuel.exampleandroidrealm.Models;
 
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by manuel on 19-04-17.
  */
 
-public class Hero {
+public class Hero extends RealmObject{
+
+    @PrimaryKey
+    private int id;
+    private String name;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- app/src/main/java/com/mengasis/manuel/exampleandroidrealm/Services/HeroService.java
+++ app/src/main/java/com/mengasis/manuel/exampleandroidrealm/Services/HeroService.java
@@ -1,8 +1,50 @@
 package com.mengasis.manuel.exampleandroidrealm.Services;
 
+import com.mengasis.manuel.exampleandroidrealm.Models.Hero;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
 /**
  * Created by manuel on 19-04-17.
  */
 
 public class HeroService {
+
+    private Realm realm;
+
+    public HeroService(Realm realm) {
+        this.realm = realm;
+    }
+
+    //CREATE
+    public void createHero(String name){
+
+        realm.beginTransaction();
+        int lastId;
+
+        if(!realm.isEmpty()){
+            lastId = realm.where(Hero.class).max("id").intValue();
+        }
+        else{
+            lastId = 0;
+        }
+
+        int nextID = lastId + 1;
+
+        Hero hero = realm.createObject(Hero.class, nextID);
+
+        hero.setName(name);
+
+        realm.commitTransaction();
+
+    }
+
+    //READ
+    public Hero[] getHeroes(){
+
+        RealmResults<Hero> results = realm.where(Hero.class).findAll();
+
+        return results.toArray(new Hero[results.size()]);
+    }
 }
