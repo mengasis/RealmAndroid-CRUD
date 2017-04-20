4ef01a351d623b0d76c707099da03c542deea120
Manuel Venegas <mengasis@outlook.com>
2017-04-19 21:41:20 -0300

    Crear y leer registros

--- app/src/main/java/com/mengasis/manuel/exampleandroidrealm/MainActivity.java
+++ app/src/main/java/com/mengasis/manuel/exampleandroidrealm/MainActivity.java
@@ -1,10 +1,19 @@
 package com.mengasis.manuel.exampleandroidrealm;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mengasis.manuel.exampleandroidrealm.Models.Hero;
+import com.mengasis.manuel.exampleandroidrealm.Services.HeroService;
+
+import java.io.File;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import io.realm.internal.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +29,17 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         Realm.setDefaultConfiguration(realmConfiguration);
+
+        HeroService heroService = new HeroService(Realm.getDefaultInstance());
+
+        heroService.createHero("Nightwing");
+
+        Hero[] heroes = heroService.getHeroes();
+
+        for (int i=0; i < heroes.length; i++){
+            Log.d("Hero: ", "ID: " + heroes[i].getId() + " - Name: " + heroes[i].getName());
+        }
+
     }
+
 }
